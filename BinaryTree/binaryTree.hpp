bf08ae42f96209a6483b42982c861ea1d449042d
Hossam Samir <eng.hsamir@gmail.com>
2019-01-29 16:46:17 +0200

    copy constructor

--- BinaryTree/binaryTree.hpp
+++ BinaryTree/binaryTree.hpp
@@ -9,7 +9,8 @@ class binaryTree
     binaryTree *right;
 
   public:
-    binaryTree(const T &t = 0) : head(t), left(0), right(0) {}
+    binaryTree(const T &t = 0) : head(t), left(0), right(0) {}                                                                         //default constructor
+    binaryTree(const binaryTree &b): head(b), left(b.left ? new binaryTree(*b.left) : 0), right(b.right?new binaryTree(*b.right:0)) {} //copy constructor
 };
 
 #endif
