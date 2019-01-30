2eb96c61ff5b54906e3261d0874f321bf4a6b948
Hossam Samir <eng.hsamir@gmail.com>
2019-01-30 13:38:40 +0200

    fix template dependency

--- Vector/vector.hpp
+++ Vector/vector.hpp
@@ -3,24 +3,25 @@
 template <class T, int N>
 class vector
 {
-  private:
-    T components[N];
+private:
+  T components[N];
 
-  public:
-    vector(const T &t = 0);         //default constructor
-    vector(const T &, int, char *); // argumet constructor
-    vector(const vector &v);        //copy constructor
+public:
+  vector(const T &t = 0);         //default constructor
+  vector(const T &, int, char *); // argumet constructor
+  vector(const vector &v);        //copy constructor
 
-    const vector &operator=(const vector &v); // assignment operator
-    const vector &operator=(const T &t);
+  const vector &operator=(const vector &v); // assignment operator
+  const vector &operator=(const T &t);
 
-    const T &operator[](int i) const { return components[i]; }
-    void set(int i, const T &a) { components[i] = a; }
+  const T &operator[](int i) const { return components[i]; }
+  void set(int i, const T &a) { components[i] = a; }
 
-    const vector &operator+=(const vector &v);
-    const vector &operator-=(const vector &v);
-    const vector &operator*=(const T &t);
-    const vector &operator/=(const T &);
-    ~vector() {}
+  const vector &operator+=(const vector &v);
+  const vector &operator-=(const vector &v);
+  const vector &operator*=(const T &t);
+  const vector &operator/=(const T &);
+  ~vector() {}
 };
+// #include "vector.cpp" // Includes the template methods file
 #endif
