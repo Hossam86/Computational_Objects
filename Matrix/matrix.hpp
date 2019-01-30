2dec03db7946f16d46d5c49389d1a59856614e1a
Hossam Samir <eng.hsamir@gmail.com>
2019-01-30 16:44:57 +0200

    include vector template

--- Matrix/matrix.hpp
+++ Matrix/matrix.hpp
@@ -1,17 +1,20 @@
 #ifndef MATRIX_H
 #define MATRIX_H
+#include "../Vector/vector.hpp"
+#include "../Vector/vector.cpp"
+
 template <class T, int M, int N>
 class matrix : public vector<vector<T, M>, N>
 {
 private:
-  T /* data */;
+  // T /* data */;
 
 public:
   matrix();
   //implicit converter
   matrix(const vector<vector<T, M>, N> &) {}
   //read matrix element
-  const T &operator()(int i, int j) const { return *this[i][j] }
+  const T &operator()(int i, int j) const { return *this[i][j]; }
 
   //update operator
   const matrix &operator+=(const matrix &);
