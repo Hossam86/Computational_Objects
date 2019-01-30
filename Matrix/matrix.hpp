4de5350c1466d406061b0f239f5e63ec7e23c25d
Hossam Samir <eng.hsamir@gmail.com>
2019-01-30 15:20:25 +0200

    update operator

--- Matrix/matrix.hpp
+++ Matrix/matrix.hpp
@@ -10,7 +10,13 @@ public:
   matrix();
   //implicit converter
   matrix(const vector<vector<T, M>, N> &) {}
-  //read matrix element 
+  //read matrix element
   const T &operator()(int i, int j) const { return *this[i][j] }
+
+  //update operator
+  const matrix &operator+=(const matrix &);
+  const matrix &operator-=(const matrix &);
+  const matrix &operator*=(const T &);
+  const matrix &operator/=(const T &);
 };
 #endif
