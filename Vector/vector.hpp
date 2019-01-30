7fffaf6f16bca5b955fda61fd010dbe07ff34003
Hossam Samir <eng.hsamir@gmail.com>
2019-01-30 02:06:08 +0200

    vector template declaration

---
+++ Vector/vector.hpp
@@ -0,0 +1,26 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+template <class T, int N>
+class vector
+{
+  private:
+    T components[N];
+
+  public:
+    vector(const T &t = 0);         //default constructor
+    vector(const T &, int, char *); // argumet constructor
+    vector(const vector &v);        //copy constructor
+
+    const vector &operator=(const vector &v); // assignment operator
+    const vector &operator=(const T &t);
+
+    const T &operator[](int i) const { return components[i]; }
+    void set(int i, const T &a) { components[i] = a; }
+
+    const vector &operator+=(const vector &v);
+    const vector &operator-=(const vector &v);
+    const vector &operator*=(const T &t);
+    const vector &operator/=(const T &);
+    ~vector() {}
+};
+#endif
