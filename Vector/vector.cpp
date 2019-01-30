95e10ab659b56fdc93a9ed896bb1485ecc3a807a
Hossam Samir <eng.hsamir@gmail.com>
2019-01-30 03:00:02 +0200

    vector constructors  implem

---
+++ Vector/vector.cpp
@@ -0,0 +1,30 @@
+#include "vector.hpp"
+//default constructor
+template <class T, int N>
+vector<T, N>::vector(const T &a)
+{
+    for (int i = 0; i < N; i++)
+    {
+        components[i] = a;
+    }
+}
+
+//constructor of standard unit vector
+template <class T, int N>
+vector<T, N>::vector(const T &t, int n, char *)
+{
+    for (int i = 0; i < N; i++)
+    {
+        components[i] = t;
+    }
+    components[n] = t;
+}
+//copy constructor
+template <class T, int N>
+vector<T, N>::vector(const vector &v)
+{
+    for (int i = 0; i < N; i++)
+    {
+        components[i] = v.components[i];
+    }
+}
