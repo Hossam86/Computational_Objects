4e48f5dcd66499d09b71c229493efa8008062a56
Hossam Samir <eng.hsamir@gmail.com>
2019-02-04 20:00:12 +0200

    print func

--- BinaryTree/binaryTree.cpp
+++ BinaryTree/binaryTree.cpp
@@ -1,10 +1,20 @@
 
 //Let us use the binary-tree object to solve the tower problem
 #include "binaryTree.hpp"
+#include "../Vector/vector.hpp"
+// print binaryTree
+template <class T>
+void print(const binaryTree<T> &b)
+{
+    if (b.readLeft())
+        print(*b.readLeft());
+    print(b());
+    if (b.readRight())
+        print(*b.readright)
+}
+typedef vector<int, 2> move;
 int main(int argc, char const *argv[])
 {
     /* code */
-    binaryTree<int> bT;
-    bT();
-    return 0;
+    binaryTree<move> tower(16);
 }
